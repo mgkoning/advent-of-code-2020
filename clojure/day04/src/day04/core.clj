b9dda5c4aac0cc55448e6dc39f97054ae0353017
Michiel Koning <m.g.koning@gmail.com>
2020-12-04 12:30:40 +0100

    Day 04: Passport Processing (Clojure, part 2)

--- clojure/day04/src/day04/core.clj
+++ clojure/day04/src/day04/core.clj
@@ -5,9 +5,13 @@
   []
   (clojure.string/split-lines (slurp "..\\..\\input\\day04.txt")))
 
+(defn words [x] (clojure.string/split x #" "))
+
+(defn key-val [x] (clojure.string/split x #":"))
+
 (defn make-passport
   [passport-lines]
-  (apply concat (map #(clojure.string/split %  #" ") passport-lines)))
+  (apply concat (map words passport-lines)))
 
 (defn read-passports
   [lines]
@@ -21,16 +25,52 @@
           (recur rest [] (conj passports (make-passport passport-lines)))
           (recur rest (conj passport-lines line) passports))))))
 
-(defn is-valid
+(defn is-valid-pass1
   [passport-items]
-  (let [has-item
-         (fn [item] (some identity (map #(clojure.string/starts-with? % item) passport-items)))]
+  (let [has-item (fn [item]
+                    (some identity (map #(clojure.string/starts-with? % item) passport-items)))]
     (every? identity (map has-item ["byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"]))))
-  
+
+(defn is-range [lo hi candidate]
+  (let [val (Integer. candidate)]
+    (and (<= lo val) (<= val hi))))
+
+(defn valid-hgt? [hgt]
+  (let [[_ val unit] (re-find #"(\d+)+(in|cm)" hgt)]
+    (cond
+      (= "in" unit) (is-range 59 76 val)
+      (= "cm" unit) (is-range 150 193 val)
+      :else false)))
+
+(def validators 
+  {"byr" (partial is-range 1920 2002)
+   "iyr" (partial is-range 2010 2020)
+   "eyr" (partial is-range 2020 2030)
+   "hgt" valid-hgt?
+   "hcl" (partial re-matches #"#[0-9a-f]{6}")
+   "ecl" (partial re-matches #"(amb|blu|brn|gry|grn|hzl|oth)")
+   "pid" (partial re-matches #"[0-9]{9}")
+   "cid" (fn [x] true)})
+
+(defn is-valid-item
+  [item]
+  (let [[key val] (key-val item)]
+    (not (not ((get validators key) val)))))
+
+(defn is-valid-pass2
+  [passport-items]
+  (every? is-valid-item passport-items))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Part 1:")
-  (let [passports (read-passports (get-input-lines))]
-    (println (count (filter identity (map is-valid passports))))))
+  (let [passports (read-passports (get-input-lines))
+        valid-pass1 (filter is-valid-pass1 passports)]
+    (println "Part 1:")
+    (println (count valid-pass1))
+    (println "Part 2:")
+    (println (count (filter is-valid-pass2 valid-pass1)))))
+
+(defn check-passport [val]
+  (let [passport (make-passport val)]
+    (and (is-valid-pass1 passport) (is-valid-pass2 passport))))
