dd6f937e99f436824ea9897776ac085eb19ab2e8
Michiel Koning <m.g.koning@gmail.com>
2020-12-05 22:43:08 +0100

    Day 05: simplication

--- clojure/day05/src/day05/core.clj
+++ clojure/day05/src/day05/core.clj
@@ -11,43 +11,54 @@
 
 (def puzzle-input (get-input-lines))
 
-(defn parse-pseudo-binary [zero one val]
-  (let [binary-string (clojure.string/replace (clojure.string/replace val one \1) zero \0)]
+(defn parse-pseudo-binary [zeroes ones val]
+  (let [binary-string 
+          (reduce #(clojure.string/replace %1 %2 \0) (reduce #(clojure.string/replace %1 %2 \1) val ones) zeroes)]
     (parse-int binary-string 2)))
 
 ; old "solution": I shouldn't take the instructions so literally
-(defn find-binary [lo hi max instructions]
-  (let [step (fn [{min :min max :max} instr]
-                (let [half (+ min (quot (- max min) 2))]
-                  (if (= lo instr) 
-                    {:min min :max half}
-                    {:min (+ half 1) :max max})))]
-    (:min (reduce step {:min 0 :max max} instructions))))
+(comment "all obsolete"
+  (defn find-binary [lo hi max instructions]
+    (let [step (fn [{min :min max :max} instr]
+                  (let [half (+ min (quot (- max min) 2))]
+                    (if (= lo instr) 
+                      {:min min :max half}
+                      {:min (+ half 1) :max max})))]
+      (:min (reduce step {:min 0 :max max} instructions))))
+  
+  (defn find-seat [instructions]
+    (find-binary \L \R 7 instructions))
 
-(defn find-seat [instructions]
-  (find-binary \L \R 7 instructions))
+  (defn find-row [instructions]
+    (find-binary \F \B 127 instructions))
 
-(defn find-row [instructions]
-  (find-binary \F \B 127 instructions))
+  (defn to-seat [seating-spec]
+    (let [row-spec (subs seating-spec 0 7)
+          seat-spec (subs seating-spec 7)
+          row (parse-pseudo-binary [\F] [\B] row-spec)
+          seat (parse-pseudo-binary [\L] [\R] seat-spec)]
+      {:row row :seat seat :id (+ seat (* row 8))}))
 
-(defn to-seat [seating-spec]
-  (let [row-spec (subs seating-spec 0 7)
-        seat-spec (subs seating-spec 7)
-        row (parse-pseudo-binary \F \B row-spec)
-        seat (parse-pseudo-binary \L \R seat-spec)]
-    {:row row :seat seat :id (+ seat (* row 8))}))
+  (defn find-missing [sorted-seats]
+    (let [[a & [b & _ :as rest]] sorted-seats
+          next (+ 1 a)]
+      (if (= b next) (recur rest) next))))
 
-(defn find-missing [sorted-seats]
-  (let [[a & [b & _ :as rest]] sorted-seats
-        next (+ 1 a)]
-    (if (= b next) (recur rest) next)))
+; "one pass" solution: determine min, max and sum of all seats, as that's all that's required for
+; the puzzle answers
+(defn one-pass [seat-specs]
+  (let [step (fn [[sum min-id max-id] seat-spec]
+                (let [seat-id (parse-pseudo-binary [\F \L] [\B \R] seat-spec)]
+                  [(+ sum seat-id) (min min-id seat-id) (max max-id seat-id)]))]
+    (reduce step [0 2048 0] seat-specs)))
 
 (defn -main
   [& args]
-  (let [seats (map to-seat puzzle-input)
-        seat-ids (map :id seats)]
+  (let [[sum min-id max-id] (one-pass puzzle-input)
+        expected-sum (apply + (range min-id (+ 1 max-id)))
+        missing-id (- expected-sum sum)]
     (println "Part 1:")
-    (println (reduce max seat-ids))
+    (println max-id)
     (println "Part 2:")
-    (println (find-missing (sort seat-ids)))))
+    (println missing-id)))
 
