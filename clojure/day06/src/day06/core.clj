2e2ef9ab0c4e50d2e4505783c4680e8587376190
Michiel Koning <m.g.koning@gmail.com>
2020-12-06 08:54:25 +0100

    Day 06: Custom Customs (Clojure)

---
+++ clojure/day06/src/day06/core.clj
@@ -0,0 +1,25 @@
+(ns day06.core
+  (:require [clojure.set])
+  (:gen-class))
+
+(defn get-input-groups
+  []
+  (clojure.string/split (slurp "..\\..\\input\\day06.txt") #"\r\n\r\n" ))
+
+(defn get-distinct-answers-count [group]
+  (count (remove #{\return \newline} (into #{} group))))
+
+(defn get-counts-sum [count-fn groups]
+  (apply + (map count-fn groups)))
+
+(defn get-common-answers-count [group]
+  (let [lines (clojure.string/split-lines group)
+        sets (map (partial into #{}) lines)
+        common-answers (apply clojure.set/intersection sets)]
+    (count common-answers)))
+
+(defn -main
+  [& args]
+  (let [puzzle-input (get-input-groups)]
+    (println (get-counts-sum get-distinct-answers-count puzzle-input))
+    (println (get-counts-sum get-common-answers-count puzzle-input))))
