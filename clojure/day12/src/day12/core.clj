6454f3561490878f054fd4b32703a912cff7f5a8
Michiel Koning <m.g.koning@gmail.com>
2020-12-12 15:58:03 +0100

    Day 12: Rain Risk (Clojure, part 2)

--- clojure/day12/src/day12/core.clj
+++ clojure/day12/src/day12/core.clj
@@ -20,38 +20,53 @@
     (= \F command) (add-vector pos [(* arg dx) (* arg dy)])
     :else pos))
 
-(def east-dir  [1  0 ])
-(def west-dir  [-1 0 ])
-(def north-dir [0  -1])
-(def south-dir [0  1 ])
+(defn left 
+  "Rotate a vector left (i.e., -90 degrees)"
+  [[dx dy]]
+  [dy (- dx)])
 
-(defn left-part1 [dir]
-  (cond
-    (= west-dir dir) south-dir
-    (= south-dir dir) east-dir
-    (= east-dir dir) north-dir
-    (= north-dir dir) west-dir))
+(defn right
+  "Rotate a vector right (i.e., 90 degrees)"
+  [[dx dy]]
+  [(- dy) dx])
 
-(defn right-part1 [dir]
-  (cond
-    (= west-dir dir) north-dir
-    (= north-dir dir) east-dir
-    (= east-dir dir) south-dir
-    (= south-dir dir) west-dir))
-
-
-(defn apply-dir-instruction-part1 [[command arg] [dx dy :as dir]]
+(defn apply-dir-instruction [[command arg] [dx dy :as dir]]
   (let [turns (quot arg 90)]
     (cond
-      (= \L command) (nth (iterate left-part1 dir) turns)
-      (= \R command) (nth (iterate right-part1 dir) turns)
+      (= \L command) (nth (iterate left dir) turns)
+      (= \R command) (nth (iterate right dir) turns)
       :else dir)))
 
 (defn follow-instruction-part1 [{ pos :pos dir :dir} instruction]
-  {:pos (apply-pos-instruction-part1 instruction pos dir) :dir (apply-dir-instruction-part1 instruction dir)})
+  {:pos (apply-pos-instruction-part1 instruction pos dir)
+   :dir (apply-dir-instruction instruction dir)})
 
 (defn follow-instructions-part1 [instructions]
-  (reduce follow-instruction-part1 {:pos [0 0] :dir east-dir} instructions))
+  (reduce follow-instruction-part1 {:pos [0 0] :dir [1 0]} instructions))
+
+(defn apply-pos-instruction-part2 [[command arg] pos [dx dy]]
+  (if
+    (= \F command) (add-vector pos [(* arg dx) (* arg dy)])
+    pos))
+
+(defn apply-waypoint-instruction [[command arg] waypoint]
+  (let [turns (quot arg 90)]
+    (cond
+      (= \N command) (add-vector waypoint [0 (- arg)])
+      (= \S command) (add-vector waypoint [0 arg])
+      (= \W command) (add-vector waypoint [(- arg) 0])
+      (= \E command) (add-vector waypoint [arg 0])
+      (= \L command) (nth (iterate left waypoint) turns)
+      (= \R command) (nth (iterate right waypoint) turns)
+      :else waypoint)))
+
+(defn follow-instruction-part2 [{pos :pos waypoint :waypoint} instruction]
+  {:pos (apply-pos-instruction-part2 instruction pos waypoint) 
+   :waypoint (apply-waypoint-instruction instruction waypoint)})
+
+(defn follow-instructions-part2 [instructions]
+  (reduce follow-instruction-part2 {:pos [0 0] :waypoint [10 -1]} instructions))
+
 
 (def instructions (map parse-instruction (get-input-lines)))
 
@@ -62,5 +77,7 @@
   (let [{[x y] :pos dir :dir} (follow-instructions-part1 instructions)]
     (println "Part 1:")
     (println (+ (abs x) (abs y)))
-    (println "Part 2:")
-    (println " .. solution .. ")))
+    (let [{[x y] :pos} (follow-instructions-part2 instructions)]
+      (println "Part 2:")
+      (println (+ (abs x) (abs y))))))
+
