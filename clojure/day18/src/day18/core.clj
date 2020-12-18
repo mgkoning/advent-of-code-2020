1a5da1dfca2f6e17467a71fe92dcb6b4e557c27d
Michiel Koning <m.g.koning@gmail.com>
2020-12-18 09:43:01 +0100

    Day 18: Operation Order (part 1)

---
+++ clojure/day18/src/day18/core.clj
@@ -0,0 +1,30 @@
+(ns day18.core
+  (:gen-class))
+
+(defn get-input-lines []
+  (clojure.string/split-lines (slurp "..\\..\\input\\day18.txt")))
+
+(def lparen (first "("))
+(def rparen (first ")"))
+
+(defn parse-expr
+  ([expr] (parse-expr expr nil nil))
+  ([[cursor & rest] acc op]
+    (let [consume (fn [val] (if (some? op) (op acc val) val))]
+      (cond
+        (or (= cursor rparen) (= cursor nil)) [acc rest]
+        (= cursor \*) (recur rest acc *)
+        (= cursor \+) (recur rest acc +)
+        (= cursor lparen)
+          (let [[subexpr remaining] (parse-expr rest)]
+            (recur remaining (consume subexpr) nil))
+        (= cursor \space) (recur rest acc op)
+        :else (recur rest (consume (Long/parseLong (str cursor))) nil)))))
+
+(defn part1 [lines]
+  (apply + (map #(first (parse-expr %)) lines)))
+
+(defn -main
+  [& args]
+  (println "Part 1:")
+  (println (part1 (get-input-lines))))
