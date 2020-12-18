276f94eaf2baf1755874fbfbd2042a410723f127
Michiel Koning <michiel@ion3.nl>
2020-12-18 12:13:26 +0100

    Day 18: Operation Order (part 2)

--- clojure/day18/src/day18/core.clj
+++ clojure/day18/src/day18/core.clj
@@ -4,29 +4,58 @@
 (defn get-input-lines []
   (clojure.string/split-lines (slurp "..\\..\\input\\day18.txt")))
 
+(defn parse-digit [c]
+  (Long/parseLong (str c)))
+
+; just here so VS Code won't screw up my paren matching :(
 (def lparen (first "("))
 (def rparen (first ")"))
 
-(defn parse-expr
-  ([expr] (parse-expr expr nil nil))
-  ([[cursor & rest] acc op]
-    (let [consume (fn [val] (if (some? op) (op acc val) val))]
-      (cond
-        (or (= cursor rparen) (= cursor nil)) [acc rest]
-        (= cursor \*) (recur rest acc *)
-        (= cursor \+) (recur rest acc +)
-        (= cursor lparen)
-          (let [[subexpr remaining] (parse-expr rest)]
-            (recur remaining (consume subexpr) nil))
-        (= cursor \space) (recur rest acc op)
-        :else (recur rest (consume (Long/parseLong (str cursor))) nil)))))
+(def operators {\+ + \* *})
+(defn is-operator? [c] (contains? operators c))
+(def precedences-p1 {\+ 1 \* 1})
+(def precedences-p2 {\+ 9 \* 1})
+(def precedences-reg {\+ 1 \* 9})
 
-(defn part1 [lines]
-  (apply + (map #(first (parse-expr %)) lines)))
+(defn apply-ops [[a b & values :as value-stack] [op & ops]]
+  (if (nil? op)
+    value-stack
+    (recur (cons ((operators op) a b) values) ops)))
+
+; simplified Shunting-yard algorithm (https://en.wikipedia.org/wiki/Shunting-yard_algorithm)
+; apply operators directly rather than collecting them for output
+(defn parse-expr-shunting [precedences expr]
+  (loop [[cursor & rest] expr
+         output-stack ()
+         op-stack ()]
+    (letfn [(take-ops-to-apply [c [op & ops :as all-ops] popped]
+              (cond
+                (= lparen op) [(reverse popped), all-ops]
+                (nil? op) [(reverse popped), ()]
+                (<= (precedences c) (precedences op)) (recur c ops (cons op popped))
+                :else [(reverse popped), all-ops]))]
+      (cond
+        (= \space cursor) (recur rest output-stack op-stack)
+        (nil? cursor) (apply-ops output-stack op-stack)
+        (= lparen cursor) (recur rest output-stack (cons lparen op-stack))
+        (= rparen cursor)
+          (let [ops-to-apply (take-while #(not= lparen %) op-stack)
+                remaining-ops (drop 1 (drop-while #(not= lparen %) op-stack))]
+            (recur rest (apply-ops output-stack ops-to-apply) remaining-ops))
+        (is-operator? cursor)
+          (let [[ops-to-apply remaining-ops] (take-ops-to-apply cursor op-stack ())]
+            (recur rest (apply-ops output-stack ops-to-apply) (cons cursor remaining-ops)))
+        :else (recur rest (cons (parse-digit cursor) output-stack) op-stack)))))
+
+(defn math-homework [precedences lines]
+  (apply + (map #(first (parse-expr-shunting precedences %)) lines)))
 
 (defn -main
   [& args]
+  (let [input-lines (get-input-lines)]
   (println "Part 1:")
-  (println (part1 (get-input-lines)))
+  (println (math-homework precedences-p1 input-lines))
   (println "Part 2:")
-  (println " ... "))
+  (println (math-homework precedences-p2 input-lines))
+  (println "Part 3 (regular precedence):")
+  (println (math-homework precedences-reg input-lines))))
